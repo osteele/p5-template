34f336363bc466ffcb49186dc62ee02d9586a437
Oliver Steele <steele@osteele.com>
2020-04-17 13:18:06 -0400

    Re-indent js, css

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
 // Code in this function is run once, when the sketch is started.
 export function setup() {
-    createCanvas(windowWidth, windowHeight)
+  createCanvas(windowWidth, windowHeight);
 }
 
 // Code in this function is run once per frame. If it draws the same thing each
 // time, the sketch is a static image. If it draws something different on
 // different frames, the sketch is an animation.
 export function draw() {
-    fill('black')
-    rect(0, 0, width, height)
+  background(200);
 }
