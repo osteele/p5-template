61e9ea7d116292157179fda12e856b8c50367dcd
Oliver Steele <steele@osteele.com>
2020-11-08 19:20:47 +0800

    Remove function comments

--- sketch.js
+++ sketch.js
@@ -1,11 +1,7 @@
-// Code in this function is run once, when the sketch is started.
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-// Code in this function is run once per frame. If it draws the same thing each
-// time, the sketch is a static image. If it draws something different on
-// different frames, the sketch is an animation.
 function draw() {
   background(200);
 }
